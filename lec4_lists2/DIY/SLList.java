e7cb19431b6caab147c180f08fa46b6d35d1c5fb
ymmath <81219735+ymmath@users.noreply.github.com>
2023-12-03 20:02:34 +0100

    not sure what the change is

--- lec4_lists2/DIY/SLList.java
+++ lec4_lists2/DIY/SLList.java
@@ -13,14 +13,22 @@ public class SLList {
         }
     }
 
-    private IntNode first; 
+    private IntNode first;
+    private int size;
+
+    public SLList() {
+        size = 0;
+        first = null;
+    }
 
     public SLList(int x) {
         first = new IntNode(x, null);
+        size = 1;
     }
 
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
+        size += 1;
         first = new IntNode(x, first);
     }    
 
@@ -32,7 +40,13 @@ public class SLList {
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
         /* Your Code Here! */
+        if (first == null) {
+            this.addFirst(x);
+            return;
+        }
+        size += 1;
         IntNode p = first;
+
 
         /* Move p until the end of the list */
         while (p.next != null) {
@@ -41,21 +55,15 @@ public class SLList {
         p.next = new IntNode(x, null);
     }
 
-    private static int size (IntNode p) {
-        if (p.next == null) {
-            return 1;
-        }
-        return 1 + size(p.next);
-    }
     /** Returns the number of items in the list using recursion. */
     public int size() {
         /* Your Code Here! */
-        return size(first);
+        return size;
     }
 
     public static void main(String[] args) {
-        SLList L = new SLList(15);
-        L.addFirst(10);
+        SLList L = new SLList();
+        L.addLast(10);
         L.addFirst(5);
         System.out.println(L.getFirst());
         System.out.println(L.size());
