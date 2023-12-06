7e3105699d36ad68bc3f74de2283c3e9ed05cd0b
Yash Mathradas <yash.mathradas@pwc.com>
2023-12-06 21:50:23 +0100

    do the SLList DIY

--- lec4_lists2/DIY/SLList.java
+++ lec4_lists2/DIY/SLList.java
@@ -1,10 +1,12 @@
 package lec4_lists2.DIY;
 
+import edu.princeton.cs.algs4.In;
+
 /** This is a fill in the blanks version of the SLList class
  *  in case you want to try to figure out how to write it yourself.
  */
 public class SLList {
-    public static class IntNode {
+    private static class IntNode {
         public int item;
         public IntNode next;
         public IntNode(int i, IntNode n) {
@@ -13,41 +15,37 @@ public class SLList {
         }
     }
 
-    private IntNode first;
+    /* The first item (if it exists) is at sentinel.next */
+    private IntNode sentinel;
     private int size;
 
+    /** Creates an empty list */
     public SLList() {
+        sentinel = new IntNode(42, null);
         size = 0;
-        first = null;
     }
 
     public SLList(int x) {
-        first = new IntNode(x, null);
+        sentinel = new IntNode(42, null);
+        sentinel.next = new IntNode(x, null);
         size = 1;
     }
 
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
         size += 1;
-        first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next);
     }    
 
     /** Retrieves the front item from the list. */
     public int getFirst() {
-        return first.item;
+        return sentinel.next.item;
     }
 
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
-        /* Your Code Here! */
-        if (first == null) {
-            this.addFirst(x);
-            return;
-        }
         size += 1;
-        IntNode p = first;
-
-
+        IntNode p = sentinel;
         /* Move p until the end of the list */
         while (p.next != null) {
             p = p.next;
@@ -57,17 +55,14 @@ public class SLList {
 
     /** Returns the number of items in the list using recursion. */
     public int size() {
-        /* Your Code Here! */
         return size;
     }
 
     public static void main(String[] args) {
-        SLList L = new SLList();
-        L.addLast(10);
-        L.addFirst(5);
-        System.out.println(L.getFirst());
-        System.out.println(L.size());
-        L.addLast(20);
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addLast(5);
+        L.addLast(3);
         System.out.println(L.size());
     }
 }
