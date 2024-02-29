affdc7f303e5959aed6b536a23e703716b373dcf
Yash <yash.mathradas@pwc.com>
2024-02-29 17:37:38 +0100

    stuff

--- lec7_lists4/DIY/AList.java
+++ lec7_lists4/DIY/AList.java
@@ -4,32 +4,55 @@ package lec7_lists4.DIY;
  *  in case you want to try to figure out how to write it yourself.
  *  After writing your methods, you can run the AListTest file.
  */
-public class AList {
+
+/* Invariants:
+addLast: The next item we want to add will go into position size
+getLast: The item we want to return is in position size - 1
+size: The number of items in the list should be size.
+ */
+public class AList<Blorp> {
     /** Creates an empty list. */
+    private int size;
+    private Blorp[] items;
     public AList() {
+        size = 0;
+        items = (Blorp[]) new Object[100];
     }
 
+    private void resize(int capacity) {
+        Blorp[] a = (Blorp[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, this.size);
+        items = a;
+    }
     /** Inserts X into the back of the list. */
-    public void addLast(int x) {
+    public void addLast(Blorp x) {
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        this.items[size] = x;
+        this.size += 1;
     }
 
     /** Returns the item from the back of the list. */
-    public int getLast() {
-        return 0;        
+    public Blorp getLast() {
+        return items[size - 1];
     }
     /** Gets the ith item in the list (0 is the front). */
-    public int get(int i) {
-        return 0;        
+    public Blorp get(int i) {
+        return items[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
-    public int removeLast() {
-        return 0;
+    public Blorp removeLast() {
+        Blorp returnItem = getLast();
+        items[size - 1] = null;
+        size -= 1;
+        return returnItem;
     }
 } 
